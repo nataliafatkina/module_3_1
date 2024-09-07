56561dd9c9493188c642dce110cd256307f17603
nataliafatkina <fatkina.natali@gmail.com>
2024-09-07 13:20:38 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return tuple([len(string), string.upper(), string.lower()])
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if i.lower() == string.lower():
+            return True
+    else:
+        return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'UrBan']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
